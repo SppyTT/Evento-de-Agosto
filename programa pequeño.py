7f2d71395f500816b0d0d3a246c29203cf48f90e
SppyTT <54282091+SppyTT@users.noreply.github.com>
2019-08-19 18:55:21 +0200

    Add files via upload
    Pequeño programa que es capaz de multiplicar por uno, dos y tres cualquier nombre natural.

---
+++ programa pequeño.py
@@ -0,0 +1,32 @@
+multiplicador3 = int(input("Introduzca el número por el cual quiere que sea multiplicado por 3: "))
+
+resultado = multiplicador3 * 3
+
+def multiplicacion3(multiplicador3, resultado):
+
+       multiplicador3 * 3
+       print (resultado)
+
+print(multiplicacion3(multiplicador3, resultado ))
+
+multiplicador2 = int(input("Introduzca el número por el cual quiere que sea multiplicado por 2: "))
+
+resultado = multiplicador2 * 2
+
+def multiplicacion2(multiplicador2, resultado):
+
+       multiplicador2 * 2
+       print (resultado)
+
+print(multiplicacion2(multiplicador2, resultado ))
+
+multiplicador = int(input("Introduzca el número por el cual quiere que sea multiplicado por 1: "))
+
+resultado = multiplicador * 1
+
+def multiplicacion(multiplicador, resultado):
+
+       multiplicador * 1
+       print (resultado)
+
+print(multiplicacion(multiplicador, resultado ))
